348d83ac549412f7ad97a14ec1c4d81fe620ce0c
Ahmed Elneziliiy <ahmedgamalgamal123456@gmail.com>
2022-08-31 17:27:05 +0200

    Add Products To Home

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db):base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var obj = _db.Products.FirstOrDefault(m => m.Id== product.Id);
+            if (obj!=null)
+            {
+                if (product.ImageUrl!=null)
+                {
+                    obj.ImageUrl = product.ImageUrl;
+                }
+                obj.ISBN = product.ISBN;
+                obj.Price = product.Price;
+                obj.Price50 = product.Price50;
+                obj.Price100 = product.Price100;
+                obj.ListPrice = product.ListPrice;
+                obj.Title = product.Title;
+                obj.Description = product.Description;
+                obj.CategoryId = product.CategoryId;
+                obj.Author = product.Author;
+                obj.CoverTypeId = product.CoverTypeId;
+
+               
+            }
+            
+        }
+
+    }
+}
---
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,177 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        
+        //GET
+        public IActionResult Upsert(int? id)//null at creating and not null on editing
+        {
+            ProductVM produtVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(m=>new SelectListItem { 
+                Text=m.Name,
+                Value=m.Id.ToString()
+                }),
+               
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(m => new SelectListItem
+                {
+                    Text = m.Name,
+                    Value = m.Id.ToString()
+                })
+            };
+
+            
+            if (id==null)
+            {
+                //Create
+                return View(produtVM); 
+            }
+            
+            //Edit
+                produtVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault()); //cause if it is null
+
+                if (produtVM.Product == null)
+                {
+                    return NotFound();
+                }
+                    
+                return View(produtVM);//for edit 
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    
+
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\products");
+                    var extenstion = Path.GetExtension(files[0].FileName);
+
+                    if (productVM.Product.ImageUrl != null)
+                    {
+                        //this is an edit and we need to remove old image
+                        
+                        var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+                    }
+                    using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extenstion),FileMode.Create))
+                    {
+                        files[0].CopyTo(filesStreams);
+                    }
+                    productVM.Product.ImageUrl = @"\images\products\" + fileName + extenstion;
+                }
+                else
+                {
+                    //update when they do not change the image
+                    if (productVM.Product.Id != 0)
+                    {
+                        Product objFromDb = _unitOfWork.Product.Get(productVM.Product.Id);
+                        productVM.Product.ImageUrl = objFromDb.ImageUrl;
+                    }
+                }
+
+
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+
+
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(m => new SelectListItem
+                {
+                    Text = m.Name,
+                    Value = m.Id.ToString()
+                });
+
+                productVM.CoverTypeList = _unitOfWork.CoverType.GetAll().Select(m => new SelectListItem
+                {
+                    Text = m.Name,
+                    Value = m.Id.ToString()
+                });
+
+                if (productVM.Product.Id!=0)
+                {
+                    //for update
+                    productVM.Product = _unitOfWork.Product.Get(productVM.Product.Id);
+                }
+            }
+            return View(productVM);
+        }
+
+        #region API CALLS
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            string webRootPath = _hostEnvironment.WebRootPath;
+            var imagePath = Path.Combine(webRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+            }
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
