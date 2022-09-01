dcf97537e7351907b1ad8d7971afdd69f566b0ff
Ahmed Elneziliiy <ahmedgamalgamal123456@gmail.com>
2022-09-01 20:48:22 +0200

    Registration

---
+++ BulkyBook/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,91 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        //GET
+        public IActionResult Upsert(int? id)//null at creating and not null on editing
+        {
+            Company company = new Company();
+            if (id==null)
+            {
+                //Create
+                return View(company); 
+            }
+            
+            //Edit
+                company = _unitOfWork.Company.Get(id.GetValueOrDefault()); //cause if it is null
+
+                if (company==null)
+                {
+                    return NotFound();
+                }
+                    
+                return View(company);//for edit 
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (company.Id==0)
+                {
+                    //create
+                    _unitOfWork.Company.Add(company);
+                    
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(company); 
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        #region API CALLS
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Company.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Company.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Company.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+
+
+    }
+}
